9d9d270cc5dd8fb35a7fe595c2a42c05287ce532
Benedek Rozemberczki <benedek.rozemberczki@gmail.com>
2019-04-29 14:00:41 +0100

    Update layers.py

--- src/layers.py
+++ src/layers.py
@@ -1,8 +1,9 @@
-import torch
 import glob
-from torch_geometric.nn import GCNConv
-from utils import graph_level_reader
+import torch
 import numpy as np
+from utils import graph_level_reader
+from torch_geometric.nn import GCNConv
+
 
 class SAGE(torch.nn.Module):
 
@@ -19,24 +20,16 @@ class SAGE(torch.nn.Module):
         self.fully_connected_2 = torch.nn.Linear(self.args.first_dense_neurons, self.args.second_dense_neurons)
 
     def forward(self, data):
-
         edges = data["edges"]
         features = data["features"]
-
         node_features_1 = torch.nn.functional.relu(self.graph_convolution_1(features, edges))
-
         node_features_2 = self.graph_convolution_2(node_features_1, edges)
-
         abstract_features_1 = torch.tanh(self.fully_connected_1(node_features_2))
         attention = torch.nn.functional.softmax(self.fully_connected_2(abstract_features_1),dim=0)
-
-
         graph_embedding = torch.mm(torch.t(attention), node_features_2)
         graph_embedding = graph_embedding.view(1,-1)
-
         penalty = torch.mm(torch.t(attention),attention)-torch.eye(self.args.second_dense_neurons)
         penalty = torch.sum(torch.norm(penalty, p=2, dim=1))
-
         return graph_embedding, penalty
 
 class MacroGCN(torch.nn.Module):
@@ -58,7 +51,6 @@ class MacroGCN(torch.nn.Module):
         predictions = torch.nn.functional.log_softmax(node_features_2,dim=1)
         return predictions
 
-
 class SEAL(torch.nn.Module):
      
     def __init__(self, args, number_of_features, number_of_labels): 
@@ -72,7 +64,6 @@ class SEAL(torch.nn.Module):
         self.graph_level_model = SAGE(self.args, self.number_of_features)
         self.hierarchical_model = MacroGCN(self.args, self.args.second_gcn_dimensions*self.args.second_dense_neurons, self.number_of_labels)
 
-
     def forward(self, graphs, macro_edges):
         embeddings = []
         penalties = 0
@@ -84,9 +75,3 @@ class SEAL(torch.nn.Module):
         penalties = penalties/len(graphs)
         predictions = self.hierarchical_model(embeddings, macro_edges)
         return predictions, penalties
-        
-        
-
-
-
-
